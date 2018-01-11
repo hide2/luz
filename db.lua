73037f9f7b6bade8e77d7a68e2eba715f2c0de68
hide2 <23771066@qq.com>
2018-01-11 19:04:47 +0800

    x

--- db.lua
+++ db.lua
@@ -1,9 +1,8 @@
-local Http = require("./luz/http").Http
-local r = require("./luz/router").new()
-local JSON = require("rapidjson")
-local env = require("luasql.sqlite3").sqlite3()
-local db = env:connect("/tmp/test.sqlite3")
+local app = require("./luz/app").app:new()
+local JSON = require('rapidjson')
+local db = require("luasql.sqlite3").sqlite3():connect("/tmp/test.sqlite3")
 
+-- prepare db
 db:execute"DROP TABLE user"
 db:execute[[
   CREATE TABLE user(
@@ -30,42 +29,13 @@ local function rows (connection, sql_statement)
   end
 end
 
-local function prepareHeader(req, body)
-	local header = {
-		code = 200,
-		{ "Server", "Luz" },
-		{ "Content-Type", "text/plain" },
-		{ "Content-Length", #body },
-	}
-	if req.keepAlive then
-		header[#header + 1] = { "Connection", "Keep-Alive" }
-	end
-	return header
-end
-
-local function onUser(params)
+app:get('/user/:id', function(params)
 	local user
 	for id, name, email in rows (db, "select id, name, email from user where id = "..params.id) do
 		user = {id = id, name = name, email = email}
 	end
-	local body = JSON.encode(user)
-	return body
-end
-
-local function dispatchRequest(client, req)
-	local body = ''
-	
-	-- dispatch request urls here
-	r:get('/user/:id', function(params)
-		body = onUser(params)
-	end)
-
-	r:execute(req.method, req.path)
-	local header = prepareHeader(req, body)
-	client:respond(header, body)
-end
-
-local server = Http:new()
-server:listen({port=8003}, dispatchRequest)
+	return JSON.encode(user)
+end)
+app:listen({port=8003})
 
 print("Http Server listening at http://0.0.0.0:8003/user/:id")
