4c47f33af7812532de1bb44e4f47b86034164640
hide2 <23771066@qq.com>
2018-01-12 19:28:47 +0800

    x

--- db.lua
+++ db.lua
@@ -1,6 +1,6 @@
 local app = require("./luz/app").app:new()
 local JSON = require('rapidjson')
-local db = require("./luz/db").DB:new():connect("/tmp/test.sqlite3")
+local db = require("./luz/db").DB:new("/tmp/test.sqlite3")
 
 -- prepare db
 db:run"DROP TABLE user"
